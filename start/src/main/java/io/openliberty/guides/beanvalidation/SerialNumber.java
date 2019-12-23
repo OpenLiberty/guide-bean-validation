6f358ae7380cfd0a985e5e50b8e02efde548faea
Nimrah Gill <Nimrah.Gill@ibm.com>
2019-12-23 16:09:11 -0500

    simplified SerialNumber.java

--- start/src/main/java/io/openliberty/guides/beanvalidation/SerialNumber.java
+++ start/src/main/java/io/openliberty/guides/beanvalidation/SerialNumber.java
@@ -1,11 +1,9 @@
 package io.openliberty.guides.beanvalidation;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
-@Documented
 public @interface SerialNumber {
 
 }
