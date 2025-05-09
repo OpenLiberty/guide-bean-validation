30c5f2c6ed46c371e9c257196a74ea47cd35f8b0
Gilbert Kwan <gkwan@ca.ibm.com>
2025-05-09 12:07:38 -0400

    update to use maven wrapper
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -8,13 +8,14 @@ set -euxo pipefail
 ##############################################################################
 
 # LMP 3.0+ goals are listed here: https://github.com/OpenLiberty/ci.maven#goals
+./mvnw -version
 
 ## Rebuild the application
 #       package                   - Take the compiled code and package it in its distributable format.
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
@@ -26,6 +27,6 @@ mvn -ntp -Dhttp.keepAlive=false \
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-mvn -ntp liberty:start
-mvn -ntp failsafe:integration-test liberty:stop
-mvn -ntp failsafe:verify
+./mvnw -ntp liberty:start
+./mvnw -ntp failsafe:integration-test liberty:stop
+./mvnw -ntp failsafe:verify
